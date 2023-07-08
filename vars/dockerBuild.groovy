755f06f4703b3db9d7d24e894e359a54dbb20285
kumar-m97 <56954202+kumar-m97@users.noreply.github.com>
2023-07-09 01:26:15 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,6 +1,7 @@
 def call() {
-	sh "docker build . -t app-server:v${env.BUILD_ID}"
-	dockerImage = sh "docker tag app-server:v${env.BUILD_ID} kumarmlive/kumar:v${env.BUILD_ID}"
+	//sh "docker build . -t app-server:v${env.BUILD_ID}"
+	//dockerImage = sh "docker tag app-server:v${env.BUILD_ID} kumarmlive/kumar:v${env.BUILD_ID}"
 	//sh "docker push kumarmlive/kumar:v${env.BUILD_ID}"
+	dockerImage = docker.build("kumarmlive/kumar:v${env.BUILD_ID}")
 	dockerImage.push()
 }
